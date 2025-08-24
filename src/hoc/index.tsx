724b741377379a86d5a3325c8719f385d97f9879
RzayevTamerlan <tamerlan.rzev@gmail.com>
2025-08-24 19:26:29 +0400

    bugfix: Add memorization to EventSubscription

--- src/hoc/index.tsx
+++ src/hoc/index.tsx
@@ -1,24 +1,23 @@
-import React, { ComponentType } from 'react';
+import { ComponentType, memo } from 'react';
 import { useEvent } from '../hooks';
 import { WithEventsProps, EventSubscriptionProps } from '../types';
 
 // HOC для подписки на события
-export const withEvents = <P extends object>(
+const withEvents = <P extends object>(
   Component: ComponentType<P>,
   subscriptions: EventSubscriptionProps[] = []
 ) => {
-  return function WithEvents(props: P & WithEventsProps) {
+  return memo(function WithEvents(props: P & WithEventsProps) {
     // Подписываемся на все переданные события
     subscriptions.forEach(({ event, handler, eventBus }) => {
       useEvent(event, handler, [], { eventBus });
     });
 
     return <Component {...props} />;
-  };
+  });
 };
 
-// Компонент для подписки на события
-export const EventSubscription: React.FC<EventSubscriptionProps> = ({
+const EventSubscription: React.FC<EventSubscriptionProps> = ({
   event,
   handler,
   eventBus
@@ -26,3 +25,10 @@ export const EventSubscription: React.FC<EventSubscriptionProps> = ({
   useEvent(event, handler, [], { eventBus });
   return null;
 };
+
+const MemoizedEventSubscription = memo(EventSubscription);
+
+export {
+  withEvents,
+  MemoizedEventSubscription as EventSubscription
+}
